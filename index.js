7349f8ab4015c170e8d3e51bf55814dfb4226df2
fanuelnalem <fanuelnalem@outlook.com>
2020-05-18 23:59:45 -0700

    new

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('./client/build'));
-}
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static('./client/build'));
+// }
 
 app.use(cors())
 app.use(express.json());
@@ -28,3 +28,27 @@ app.use("/" ,myroute)
 
 
 app.listen(PORT, () => console.log('Port started on port: ' + PORT));
+
+// const express = require('express');
+// const myroute = require('./routes/userRoutes/user')
+// const mysql = require('mysql')
+// const cors = require('cors')
+ 
+// const PORT = process.env.PORT || 3001;
+
+// const app = express();
+
+
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static('./client/build'));
+// }
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// // Declare routes right here.
+ 
+// // Prepend / to any route declared inside of routes
+// app.use("/" ,myroute)
+
+// app.listen(PORT, () => console.log('Port started on port: ' + PORT));
