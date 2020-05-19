beba6ed4713c2830f466e0b1531f6729c9fb5616
fanuelnalem <fanuelnalem@outlook.com>
2020-05-19 00:08:17 -0700

    process.env.

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: 'zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        host: process.env.HOST,
         port: 3306,
-        user: 'gmridpuo8yzx3m17',
-        password: 'khfrii7b56xj14fl',
-        database: 'ep1eke73vidmeb6u'
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
     });
 }
 
