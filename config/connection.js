5e155e702aa3c4702422072966356d076603687f
fanuelnalem <fanuelnalem@outlook.com>
2020-05-20 21:49:08 -0700

    new

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,31 @@ const mysql = require('mysql');
 let connection;
 
 
+
+
 if(process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-        host: 'zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        port: 3306,
-        user: 'gmridpuo8yzx3m17',
-        password: 'khfrii7b56xj14fl',
-        database: 'ep1eke73vidmeb6u'
-    });
+  connection = mysql.createConnection({
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      password: 'password',
+      database: 'Users_DB'
+  });
 }
+
+// if(process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     connection = mysql.createConnection({
+//         host: 'zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//         port: 3306,
+//         user: 'gmridpuo8yzx3m17',
+//         password: 'khfrii7b56xj14fl',
+//         database: 'ep1eke73vidmeb6u'
+//     });
+// }
 
 
 // if(process.env.JAWSDB_URL) {
