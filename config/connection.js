f58d5eecd3858d855f0c907dc21626d308d593de
fanuelnalem <fanuelnalem@outlook.com>
2020-05-26 19:08:34 -0700

    cs

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,6 @@ if(process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
       host: 'localhost',
-      port: 3306,
       user: 'root',
       password: 'password',
       database: 'Users_DB'
